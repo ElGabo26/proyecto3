52823fda9aba2833dcc584308127e30afe9f019a
ElGabo26 <jcgab2009@gmail.com>
2026-01-28 00:31:55 -0500

    hi

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ from tools.readDocumentsPath import get_file_content_from_path
 from tools.makeContext import limpiar_respuesta_deepseek, build_prompt
 from tools.workPaths import openPath
 
+
 def getResponse(rootPath:str,client,modelName,temperature:float):
     resultado={"model":modelName,"temperature":temperature,
                "context":"","prompt":"","answer":""}
     archivos=openPath(rootPath)
-    contexto_documentos = f"DE UN  TOTAL DE:{len(archivos[-1])} DOCUEMTOS CON LA SIGUIENTE INFORMACION\n"
+    contexto_documentos = f"DE UN  TOTAL DE:{len(archivos)} DOCUMENTOS CON LA SIGUIENTE INFORMACION"
     
     for archivo in archivos[-1]:
-        contexto_documentos +=f"DOCUMENTO DE NOMBRE:{archivo}\nCONTENIDO:\n"
+        contexto_documentos +=f"DOCUMENTO DE NOMBRE:{archivo.split(".")[0]}\nCONTENIDO:\n"
         contexto_documentos += get_file_content_from_path(f"{rootPath}/{archivo}")
-        contexto_documentos += "\n"
         
     system_instruction = build_prompt(contexto_documentos)
     resultado['context']=system_instruction
     resultado['prompt']=contexto_documentos
-    t0=time()
+    
     try:
-        
+        print(modelName)
+        t0=time()    
         response = client.chat.completions.create(
         model=modelName,
         messages=[
@@ -33,10 +34,11 @@ def getResponse(rootPath:str,client,modelName,temperature:float):
         temperature=temperature,
     )
         resultado['answer'] = response.choices[0].message.content
-    
+        t1=time()
     except Exception as e:
+        t1, t0=0,0
         resultado["answer"]= f"Error en el servidor local: {str(e)}"
-    t1=time()
+    
     resultado['time']=t1-t0
     resultado['caso']=rootPath
     return resultado
@@ -48,8 +50,7 @@ client = OpenAI(
     api_key="ollama",
 )
 # MODELOS
-MODELO_D = "deepseek-r1:7b"
-MODELO_L='llama3.1'
+models=["llama3.1:8b","llama3.2:3b","deepseek-r1:1.5b","deepseek-r1:1.7b"],
 
 
 correcto="./benchMark/correcto"
@@ -60,49 +61,40 @@ correctoFiles=[f"{correcto}/{x}" for x in correctoFiles]
 
 incorrectoFiles=os.listdir(incorrecto)
 incorrectoFiles=[f"{incorrecto}/{x}" for x in incorrectoFiles]
+
 paths=correctoFiles+incorrectoFiles
 
 trys=5
 
 if __name__ == '__main__':
-    print(paths)
     final=[] 
     for i in range(trys):
+        r=[]
         print(f"INTENTO {i+1}")
-        try:
-            print(f"MODELO {MODELO_D}")
-            r=list(map(lambda x: getResponse(x,
-                                         client,
-                                         MODELO_D,
-                                         0.1),
-                   paths))
-            resultD=pd.DataFrame(r)
-            resultD['model']=MODELO_D
-            
-        except Exception as e:
-            print("ERROR")
-            print(e)
-            resultD=pd.DataFrame()
-        try:
-            print(f"MODELO {MODELO_L}")
-            r1=list(map(lambda x: getResponse(x,
-                                         client,
-                                         MODELO_L,
-                                         0.1),
-                   paths))
-            resultL=pd.DataFrame(r)
-            resultL['model']=MODELO_L
-            
-        except Exception as e:
-            print("ERROR")
-            print(e)
-            resultL=pd.DataFrame()
-            
-        results=pd.concat([resultD,resultL])
+        for MODELO_D  in models:
+            print(MODELO_D)
+            try:
+                print(f"MODELO {MODELO_D}")
+                r=list(map(lambda x: getResponse(x,
+                                            client,
+                                            MODELO_D,
+                                            0.1),
+                    paths))
+                resultD=pd.DataFrame(r)
+                resultD['model']=MODELO_D
+                
+                
+            except Exception as e:
+                print("ERROR")
+                print(e)
+                resultD=pd.DataFrame()
+            r.append(resultD)
+                
+        results=pd.concat(r)
         results['try']=i
         final.append(results)
     
-    final=pd.concat(final)
+    final=pd.concat(final) 
     final.to_csv("./resultados.csv")
         
         
