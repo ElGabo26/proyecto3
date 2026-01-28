26c3e546b322dcdda0e5697bf6c78b884e52277c
ElGabo26 <134992634+ElGabo26@users.noreply.github.com>
2026-01-28 01:04:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ client = OpenAI(
     api_key="ollama",
 )
 # MODELOS
-models=["llama3.1:8b","llama3.2:3b","deepseek-r1:1.5b","deepseek-r1:1.7b"],
+models=["llama3.1:8b","llama3.2:3b","deepseek-r1:1.5b","deepseek-r1:1.7b"]
 
 
 correcto="./benchMark/correcto"
